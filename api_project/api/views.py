d963eb2956b71950d454ec6360b227cfae82dead
AlvinOkoti <alvin.okoti@gmail.com>
2025-08-03 23:31:45 +0300

    urls setup

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission: Read allowed for anyone, write allowed for admins only.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+            return True
+        return request.user and request.user.is_staff
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard actions
+    for listing, creating, retrieving, updating, and deleting Books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
