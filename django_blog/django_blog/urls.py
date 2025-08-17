af7c40bddd209933f1c83ffd11f8b3fb6b5bd55f
AlvinOkoti <alvin.okoti@gmail.com>
2025-08-17 22:22:45 +0300

    Comment functionality to blog post

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include 
+from . import views
+from .views import (
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
+)
 
 urlpatterns = [
     path("register/", views.register, name="register"),
