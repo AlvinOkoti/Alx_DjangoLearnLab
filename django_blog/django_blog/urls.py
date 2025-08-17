1e353dfa318c8c5b24ea9504010d0bb37d3d94ee
AlvinOkoti <alvin.okoti@gmail.com>
2025-08-17 08:48:21 +0300

    Blog user Authentication system

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
     path('admin/', admin.site.urls),
+    path("", views.home, name="blog-home"),
     path('', include('blog.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='post-list'), name='logout'),
