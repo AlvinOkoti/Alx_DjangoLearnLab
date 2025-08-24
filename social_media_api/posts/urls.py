a62c6b7511031773fc40933b784c7623b70c7b3e
AlvinOkoti <alvin.okoti@gmail.com>
2025-08-24 22:35:13 +0300

    Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+from django.contrib import admin
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename="post")
+router.register(r'comments', CommentViewSet, basename="comment")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),  # auth routes
+    path('api/', include('posts.urls')),  # posts and comments route
+]
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,11 @@ REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES': [
 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 ],
+
+#new aetup
+ "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,  # adjust as needed
+    "DEFAULT_FILTER_BACKENDS": ["rest_framework.filters.SearchFilter"],
 }
 
 import os
